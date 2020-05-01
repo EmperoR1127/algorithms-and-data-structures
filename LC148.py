583813e6dd4faa571b268d85a93e047666662c65
EmperoR <51844517+EmperoR1127@users.noreply.github.com>
2020-05-01 16:47:34 -0400

    review

--- LC148.py
+++ LC148.py
@@ -5,13 +5,14 @@ Approach 1: Divide and Conquer
 Intuition
 The time complexity is a typical hint of divide and conquer, plus we could utilize pointers to satisfy the space constraint.
 Similar as merge sort, recursively divide the linked list into two halves until the list has at most two nodes. 
-Swap the two nodes with respect to their values and return the heads, or just return the head if only one node in the list.
-Combine the lists and keep track the new head using a dummy node.
+Swap the two nodes with respect to their values and return the heads, or just return the head if only at most one node 
+in the list.
+Combine the lists and store the new head using a dummy node.
 
 However, unlike array, how can we efficiently locate the middle node in the list? We could use the two-pointer technique here.
-Initialize two pointer, slow and fast, respectively, and they both point to the head of the given list.
+Initialize two pointers, slow and fast, respectively, and they both point to the head of the given list.
 Move fast to two nodes after its current position, if possible. In the meantime, move slow only one node along the list.
-When fast reaches the end of the list, slow is in the middle of the list, since fast always moves two times of nodes 
+When fast reaches the end of the list, slow is in the middle of the list, since fast always moves two times number of nodes 
 compared to slow.
 
 Restricted to the constant space complexity, we can't initialize another linked list to merge two lists, 
@@ -21,9 +22,9 @@ Complexity Analysis
 
 Time complexity: O(nlogn). 
 Similar as the merge sort. The base cases are trivally sorted. In the merge part, we need to sort two lists with at most 
-n/2 nodes and combine n nodes together, with O(1) step each.
-T(n) = 2T(n/2) + O(n), T(n) = O(nlogn) according to master theorem.
-Space complexity: O(1). Only a constant number of pointer are used to divide and combine the linked list.
+n/2 nodes and combine n nodes together, with O(1) steps each.
+T(n) = 2T(n/2) + O(n), T(n) = O(nlogn) according to the master theorem.
+Space complexity: O(1). Only a constant number of pointers are initialized to divide and combine the linked list.
 """
 # Definition for singly-linked list.
 # class ListNode:
