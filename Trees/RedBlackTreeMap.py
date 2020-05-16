22436a5bf1ec6a20fbc51fc0cf4d718f7cb72229
EmperoR <lvoldemort1127@hotmail.com>
2020-05-16 15:35:14 -0400

    add deletion method for red black tree

--- Trees/RedBlackTreeMap.py
+++ Trees/RedBlackTreeMap.py
@@ -52,7 +52,52 @@ class RedBlackTreeMap(TreeMap):
                     self._set_black(parent) # set parent to black
                     self._set_black(uncle) # set uncle to black
                     self._resolve_red(grand) # recur at red grandparent
+
+    #------------------------- support for deletions -------------------------
+    def _rebalance_delete(self, p):
+        if len(self) == 1:
+            self._set_black(self.root()) # make sure the root is black
+        elif p != None:
+            n = self.num_children(p)
+            if n == 1: # deficit exists unless child is a red leaf
+                c = next(self.children(p))
+                if not self._is_red_leaf(c):
+                    self._fix_deficit(p, c)
+            elif n == 2: # removed black node with red child
+                # make the promoted child black
+                if self._is_red_leaf(self.left(p)):
+                    self._set_black(self.left(p))
+                else:
+                    self._set_black(self.right(p))
+
+    def _fix_deficit(self, z, y):
+        """Resolve black deficit at z, where y is the root of z s heavier subtree"""
+        if not self._is_red(y): # y is black, case 1 or 2
+            x = self._get_red_child(y)
+            if x != None: # y has a red child, case 1
+                old_color = self._is_red(z)
+                middle = self._restructure(x)
+                self._set_color(middle, old_color) # set middle the color of z
+                # set both children black, resolve the deficit
+                self._set_black(self.left(middle))
+                self._set_black(self.right(middle))
+            else: # both children of y is black or None, case 2
+                self._set_red(y) # set y red
+                if self._is_red(z): # if z is red, set black, resolve the deficit
+                    self._set_black(z)
+                elif not self.is_root(z): # z is originally black and not the root
+                    # propogate the dificit to higher level
+                    self._fix_deficit(self.parent(z), self.sibling(z)) 
+        else: # y is red, case 3
+            self._rotate(y)
+            self._set_black(y)
+            self._set_red(z)
+            if z == self.left(y): # one more case 1 or 2 to resolve the deficit
+                self._fix_deficit(z, self.right(z))
+            else:
+                self._fix_deficit(z, self.left(z))
             
+                
 
 
 
