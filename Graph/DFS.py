e6e6174f3748cd99f03d642cec6347052be63a83
EmperoR <lvoldemort1127@hotmail.com>
2020-07-07 13:54:07 -0400

    add dfs forest

--- Graph/DFS.py
+++ Graph/DFS.py
@@ -21,6 +21,18 @@ def construct_path(u, v, discovered):
             path.append(prev)
             curr = prev
         return reversed(path) # reorient path from u to v
+
+def DFS_component(g):
+    """Perform DFS for entire graph and return forest as a dictionary
+    Result maps each vertex v to the edge that was used to discover it.
+    (Vertices that are roots of a DFS tree are mapped to None.)
+    """
+    forest = {}
+    for u in g.vertices():
+        if u not in forest:
+            forest[u] = None # u will be the root of a tree
+            DFS(g, u, forest)
+    return forest
     
             
     
